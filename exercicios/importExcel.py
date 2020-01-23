bfbb56e1c15ebb61f8385ca2ead419ea4b9433d6
gkaustchr <gabrielkaustchr@gmail.com>
2020-01-23 13:24:31 -0300

    Adicionanod Graficos

--- exercicios/importExcel.py
+++ exercicios/importExcel.py
@@ -3,7 +3,19 @@ import matplotlib.pyplot as plt
 
 x = pd.read_excel(r"D:\Kaustchr\Documentos\acao.xlsx")
 print(x)
+#Grafico de Pontos
 plt.plot(x['Date'], x['Open'])
+plt.title(x['Ticket'][1])
+plt.show()
+
+#Grafico de Barras
+plt.hist( x['Open'], bins = 10)
+plt.title(x['Ticket'][1])
+plt.show()
+
+
+#Grafico de Pizza
+plt.pie(x['Open'])#labels="nome da fatia"
 plt.title(x['Ticket'][1])
 plt.show()
 
