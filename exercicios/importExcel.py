7760f4787ab7da0b73e19c9c9213c6c3e234313c
gkaustchr <gabrielkaustchr@gmail.com>
2020-01-23 13:14:05 -0300

    Modificando Grafico

--- exercicios/importExcel.py
+++ exercicios/importExcel.py
@@ -2,9 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 x = pd.read_excel(r"D:\Kaustchr\Documentos\acao.xlsx")
-plt.plot(x)
-plt.show();
-
+print(x)
+plt.plot(x['Date'], x['Open'])
+plt.title(x['Ticket'][1])
+plt.show()
 
 
 
